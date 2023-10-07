94c083f83808467671f9c703b0a7e8acf198e2b2
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2023-10-07 12:33:35 -0400

    adding expected result

--- spec/t/command_spec.rb
+++ spec/t/command_spec.rb
@@ -3,20 +3,27 @@
 require 'spec_helper'
 
 RSpec.describe T::Command do
-  subject(:test_class) do
-    Class.new do
-      require 't/command'
-
-      include T::Command
-    end
-  end
-
   it 'has a version number' do
     expect(T::Command::VERSION).to be_a(String)
   end
 
   describe 'A Command class' do
+    subject(:test_class) do
+      Class.new do
+        require 't/command'
+        include T::Command
+
+        def call
+          context.foo = :bar
+        end
+      end
+    end
+
     it { is_expected.to respond_to(:call) }
     it { is_expected.to respond_to(:context) }
+
+    it 'returns a context' do
+      expect(test_class.call).to be_a(T::Context)
+    end
   end
 end
