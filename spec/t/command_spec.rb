4c0edf0a8f4d87ed6a58a0bebae4e312a8a279a4
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2023-10-07 12:38:25 -0400

    adding failure modes

--- spec/t/command_spec.rb
+++ spec/t/command_spec.rb
@@ -22,8 +22,41 @@ RSpec.describe T::Command do
     it { is_expected.to respond_to(:call) }
     it { is_expected.to respond_to(:context) }
 
-    it 'returns a context' do
+    it 'returns a T::Context' do
       expect(test_class.call).to be_a(T::Context)
+    end
+
+    it 'is successful' do
+      expect(test_class.call).to be_success
+    end
+
+    it 'is not a failure' do
+      expect(test_class.call).not_to be_failure
+    end
+  end
+
+  describe 'A failing Command class' do
+    subject(:test_class) do
+      Class.new do
+        require 't/command'
+        include T::Command
+
+        def call
+          non_existing_command
+        end
+      end
+    end
+
+    it 'returns a T::Context with a non-empty errors object' do
+      expect(test_class.call.errors).not_to be_empty
+    end
+
+    it 'is not successful' do
+      expect(test_class.call).not_to be_success
+    end
+
+    it 'is a failure' do
+      expect(test_class.call).to be_failure
     end
   end
 end
