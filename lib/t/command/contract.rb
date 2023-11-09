608f4c0885251221633df48dc06af6b572f40e4c
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2023-11-09 12:09:29 -0500

    enforcing strong parameters for `allows` and `requires`

--- lib/t/command/contract.rb
+++ lib/t/command/contract.rb
@@ -13,19 +13,27 @@ module T
 
     module ClassMethods
       def allows(*attributes, **strong_attributes)
-        delegate(*attributes, to: :context)
-        strong_attributes.each_key { |strong_attribute| delegate(strong_attribute, to: :context) }
+        delegate(*attributes, to: :context) if attributes.present?
+        return if strong_attributes.blank?
 
+        strong_attributes.each_key { |strng_attr| delegate(strng_attr, to: :context) }
         validates_each strong_attributes.keys do |record, attr_name, value|
           next if value.blank?
 
           type = strong_attributes[attr_name].name.downcase.to_sym
-          record.errors.add attr_name, "is not of type #{type}" unless value.acts_like?(type)
+          record.errors.add attr_name, "does not act like #{type}" unless value.acts_like?(type)
         end
       end
 
-      def requires(*attributes)
-        validate_attributes(attributes)
+      def requires(*attributes, **strong_attributes)
+        validate_and_delegate_attributes(attributes) if attributes.present?
+
+        return if strong_attributes.blank?
+        strong_attributes.each_key { |strng_attr| delegate(strng_attr, to: :context) }
+        validates_each strong_attributes.keys do |record, attr_name, value|
+          type = strong_attributes[attr_name].name.downcase.to_sym
+          record.errors.add attr_name, "does not act like #{type}" unless value.acts_like?(type)
+        end
       end
 
       def returns(*attributes)
@@ -34,7 +42,7 @@ module T
 
       private
 
-      def validate_attributes(attributes)
+      def validate_and_delegate_attributes(attributes)
         validates(*attributes, presence: true)
         delegate(*attributes, to: :context)
       end
--- spec/t/contract_spec.rb
+++ spec/t/contract_spec.rb
@@ -4,47 +4,90 @@ require 'spec_helper'
 
 RSpec.describe T::Contract do
   describe 'A Contract class' do
-    let(:test_class) do
-      Class.new(Object) do
+    before do
+      base_contract = Class.new(Object) do
         require 't/command'
         include T::Command
         include T::Contract
 
-        def self.model_name
-          ActiveModel::Name.new(self, nil, 'temp')
-        end
-
-        requires :bar
-        allows foo: String
-        # returns :baz
-
-        def call
-          context.baz = context.bar
-        end
+        def call; end
       end
+      stub_const('BaseContract', base_contract)
     end
 
-    context 'with required variables' do
-      it 'fails if a required variable is not given' do
-        expect(test_class.call).to be_failure
+    context 'with a required variable' do
+      context 'without type specification' do
+        let(:test_class) do
+          Class.new(BaseContract) do
+            requires :bar
+          end
+        end
+
+        it 'fails if variable is not given' do
+          expect(test_class.call).to be_failure
+        end
+
+        it 'succeeds if variable is given' do
+          expect(test_class.call(bar: true)).to be_success
+        end
       end
 
-      it 'succeeds if the required variable is given' do
-        expect(test_class.call(bar: true)).to be_success
+      context 'with type specification' do
+        let(:test_class) do
+          Class.new(BaseContract) do
+            requires bar: String
+          end
+        end
+
+        it 'fails if variable is not given' do
+          expect(test_class.call).to be_failure
+        end
+
+        it 'succeeds if variable is given and type correct' do
+          expect(test_class.call(bar: 'a')).to be_success
+        end
+
+        it 'fails if variable is given and type wrong' do
+          expect(test_class.call(bar: 1)).to be_failure
+        end
       end
     end
 
     context 'with allowed variables' do
-      it 'succeeds if the allowed variable is not given' do
-        expect(test_class.call(bar: true)).to be_success
+      context 'without type specification' do
+        let(:test_class) do
+          Class.new(BaseContract) do
+            allows :bar
+          end
+        end
+
+        it 'succeeds if the allowed variable is not given' do
+          expect(test_class.call).to be_success
+        end
+
+        it 'succeeds if the allowed variable is given' do
+          expect(test_class.call(bar: true)).to be_success
+        end
       end
 
-      it 'succeeds if the allowed variable is given' do
-        expect(test_class.call(bar: true, foo: 'a')).to be_success
-      end
+      context 'with type specification' do
+        let(:test_class) do
+          Class.new(BaseContract) do
+            allows bar: String
+          end
+        end
 
-      it 'fails if the allowed variable is of the wrong type' do
-        expect(test_class.call(bar: true, foo: 1)).to be_failure
+        it 'succeeds if variable is not given' do
+          expect(test_class.call).to be_success
+        end
+
+        it 'succeeds if variable is given and type correct' do
+          expect(test_class.call(bar: 'a')).to be_success
+        end
+
+        it 'fails if variable is given and type wrong' do
+          expect(test_class.call(bar: 1)).to be_failure
+        end
       end
     end
   end
