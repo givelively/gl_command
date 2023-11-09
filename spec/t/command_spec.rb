412d0003723fcfdac5f5ab163fa136adb405455a
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2023-11-09 17:12:23 -0500

    adding specs for delegation

--- spec/t/command_spec.rb
+++ spec/t/command_spec.rb
@@ -8,63 +8,121 @@ RSpec.describe T::Command do
   end
 
   describe 'A Command class' do
-    subject(:test_class) do
-      Class.new do
+    before do
+      base_command = Class.new(Object) do
         require 't/command'
         include T::Command
+        include T::Contract
 
-        def call
-          context.foo = :bar
+        def call; end
+      end
+      stub_const('BaseCommand', base_command)
+    end
+
+    context 'when entering happy path' do
+      subject(:test_class) do
+        Class.new(BaseCommand) do
+          def call
+            context.foo = :bar
+          end
         end
+      end
+
+      it { is_expected.to respond_to(:call) }
+      it { is_expected.to respond_to(:context) }
+
+      it 'returns a T::Context' do
+        expect(test_class.call).to be_a(T::Context)
+      end
+
+      it 'is successful' do
+        expect(test_class.call).to be_successful
+      end
+
+      it 'is not a failure' do
+        expect(test_class.call).not_to be_failure
       end
     end
 
-    it { is_expected.to respond_to(:call) }
-    it { is_expected.to respond_to(:context) }
+    describe 'A failing Command class' do
+      subject(:test_class) do
+        Class.new(BaseCommand) do
+          def call
+            non_existing_command
+          end
 
-    it 'returns a T::Context' do
-      expect(test_class.call).to be_a(T::Context)
-    end
-
-    it 'is successful' do
-      expect(test_class.call).to be_successful
-    end
-
-    it 'is not a failure' do
-      expect(test_class.call).not_to be_failure
-    end
-  end
-
-  describe 'A failing Command class' do
-    subject(:test_class) do
-      Class.new do
-        require 't/command'
-        include T::Command
-
-        def call
-          non_existing_command
+          def rollback
+            context.rolled_back = true
+          end
         end
+      end
 
-        def rollback
-          context.rolled_back = true
-        end
+      it 'returns a T::Context with a non-empty errors object' do
+        expect(test_class.call.errors).not_to be_empty
+      end
+
+      it 'is not successful' do
+        expect(test_class.call).not_to be_successful
+      end
+
+      it 'is a failure' do
+        expect(test_class.call).to be_failure
+      end
+
+      it 'calls `:rollback`' do
+        expect(test_class.call(rolled_back: false).rolled_back).to be(true)
       end
     end
 
-    it 'returns a T::Context with a non-empty errors object' do
-      expect(test_class.call.errors).not_to be_empty
-    end
+    describe 'Delegation' do
+      context 'when using :requires' do
+        subject(:test_class) do
+          Class.new(BaseCommand) do
+            requires :foo
 
-    it 'is not successful' do
-      expect(test_class.call).not_to be_successful
-    end
+            def call
+              raise if foo.blank?
+            end
+          end
+        end
 
-    it 'is a failure' do
-      expect(test_class.call).to be_failure
-    end
+        it 'delegates variable to the context' do
+          expect(test_class.call(foo: :bar)).to be_successful
+        end
+      end
 
-    it 'calls `:rollback`' do
-      expect(test_class.call(rolled_back: false).rolled_back).to be(true)
+      context 'when using :allows' do
+        subject(:test_class) do
+          Class.new(BaseCommand) do
+            allows :foo
+
+            def call
+              raise if foo.blank?
+            end
+          end
+        end
+
+        it 'delegates variable to the context' do
+          expect(test_class.call(foo: :bar)).to be_successful
+        end
+      end
+
+      context 'when using :returns' do
+        subject(:test_class) do
+          Class.new(BaseCommand) do
+            returns :foo
+
+            def call
+              context.foo = :bar
+              raise if foo.blank?
+            end
+          end
+        end
+
+        it 'delegates variable to the context' do
+          expect(test_class.call).to be_successful
+        end
+      end
     end
   end
 end
