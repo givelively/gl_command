bcd649fddd6377d9cef3f20cd9229a829fd67204
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2023-11-09 16:54:32 -0500

    adding return contract verification

--- lib/t/command/contract.rb
+++ lib/t/command/contract.rb
@@ -8,6 +8,7 @@ module T
         extend ClassMethods
 
         before :validate_contract!
+        after :validate_return_contract!
       end
     end
 
@@ -26,9 +27,12 @@ module T
       end
 
       def requires(*attributes, **strong_attributes)
-        validate_and_delegate_attributes(attributes) if attributes.present?
-
+        if attributes.present?
+          validates(*attributes, presence: true)
+          delegate(*attributes, to: :context)
+        end
         return if strong_attributes.blank?
+
         strong_attributes.each_key { |strng_attr| delegate(strng_attr, to: :context) }
         validates_each strong_attributes.keys do |record, attr_name, value|
           type = strong_attributes[attr_name].name.downcase.to_sym
@@ -36,15 +40,11 @@ module T
         end
       end
 
-      def returns(*attributes)
+      def returns(*attributes, **strong_attributes)
         delegate(*attributes, to: :context)
-      end
-
-      private
-
-      def validate_and_delegate_attributes(attributes)
-        validates(*attributes, presence: true)
-        delegate(*attributes, to: :context)
+        strong_attributes.each_key { |strng_attr| delegate(strng_attr, to: :context) }
+        @return_attributes = attributes
+        @return_strong_attributes = strong_attributes
       end
     end
 
@@ -53,6 +53,42 @@ module T
 
       context.errors.copy!(errors)
       raise ContractFailure
+    end
+
+    def validate_return_contract!
+      validate_return_attributes!
+      validate_strong_return_attributes!
+    end
+
+    def strong_attributes
+      self.class.instance_variable_get(:@return_strong_attributes) || []
+    end
+
+    def validate_return_attributes!
+      attributes = self.class.instance_variable_get(:@return_attributes)
+      return if attributes.blank?
+
+      attributes.each do |attribute|
+        next if context[attribute].present?
+
+        record.errors.add attribute, 'is not being returned'
+        context.errors.copy!(errors)
+        raise ContractFailure
+      end
+    end
+
+    def validate_strong_return_attributes!
+      strong_attributes.each do |attribute, type|
+        if context[attribute].blank?
+          context.errors.add strong_attribute, 'is not being returned'
+          raise ContractFailure
+        end
+        type = type.name.downcase.to_sym
+        next if context[attribute].acts_like?(type)
+
+        context.errors.add attribute, "is returned, but does not act like a #{type}"
+        raise ContractFailure
+      end
     end
   end
 end
