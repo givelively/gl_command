ffe4bd85ace4da787e3db4d86a2ebe5827a96fa9
Seth Herr <seth.william.herr@gmail.com>
2025-03-17 18:18:13 -0700

    actually commit the update

--- lib/gl_command/callable.rb
+++ lib/gl_command/callable.rb
@@ -6,6 +6,7 @@ require 'gl_command/validatable'
 
 module GLCommand
   class Callable
+    ALWAYS_RAISE_ERRORS = ENV['GL_COMMAND_ALWAYS_RAISE'] == 'true'
     DEFAULT_OPTS = { raise_errors: false, skip_unknown_parameters: true, in_chain: false }.freeze
     RESERVED_WORDS = (DEFAULT_OPTS.keys + GLCommand::ChainableContext.reserved_words).sort.freeze
 
@@ -27,6 +28,7 @@ module GLCommand
         # (rather than in context initialize) to make errors more legible
         opts = DEFAULT_OPTS.merge(raise_errors: args.delete(:raise_errors),
                                   in_chain: args.delete(:in_chain)).compact
+        opts[:raise_errors] = true if ALWAYS_RAISE_ERRORS
         # args are passed in in perform_call(args) so that invalid args raise in a legible place
         new(build_context(**args.merge(opts))).perform_call(args)
       end
--- spec/lib/gl_command/callable_spec.rb
+++ spec/lib/gl_command/callable_spec.rb
@@ -61,6 +61,7 @@ RSpec.describe GLCommand::Callable do
             expect_any_instance_of(ArrayAdd).to receive(:instrument_command).with(:before_call).once
             expect_any_instance_of(ArrayAdd)
               .to receive(:instrument_command).with(:before_rollback).once
+            expect(GLExceptionNotifier).not_to receive(:call)
             result = ArrayAdd.call(array:, item: 6)
             failure_expectations(result)
             expect(result).to be_raise_error
